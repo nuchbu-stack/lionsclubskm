cdba903e339c99e25e09a72e6c4998401967aca0
nuchbu-stack <nuch.bu@gmail.com>
2025-09-21 17:42:36 +0700

    nav

--- script.js
+++ script.js
@@ -15,3 +15,12 @@ counters.forEach(counter => {
   };
   updateCounter();
 });
+
+window.addEventListener("scroll", function () {
+  const navbar = document.querySelector(".navbar");
+  if (window.scrollY > 50) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
